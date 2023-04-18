dcc3325ba2050b6de12e5b4417f44c123db34c86
ViktorJMarton <viktor@appmaintainers.hu>
2023-04-19 00:49:00 +0200

    config initialization, database setup

---
+++ db/Database.php
@@ -0,0 +1,51 @@
+<?php
+
+class Database{
+    private $db_name;
+    private $user;
+    private $pwd;
+    private $port;
+    private $host;
+    private static $db;
+    
+  
+    public function __construct() {
+      $config = parse_ini_file('init/config.ini');
+     
+     echo $this->db_name = $config['dbname'];
+     echo $this->user = $config['user'];
+     echo $this->pwd = $config['password'];
+     echo $this->port =$config['port'];
+     echo $this->host =$config['host'];
+
+      error_reporting(E_ALL);
+      ini_set('display_errors', '1');
+    }
+
+    public function connect() {
+        $db = pg_connect($this->getConnectionString()) 
+                or die('Belépés nem sikerült. Hiba: ' . pg_last_error());
+
+    }
+  
+    public function getDbName() {
+      return $this->db_name;
+    }
+
+    public function getUser() {
+      return $this->user;
+    }
+
+    public function getPwd() {
+      return "A Jelszo megtekintese letiltva";
+    }
+  
+    protected function  getConnectionString() {
+        return $connection_string = "host={$this->host}
+                                     port={$this->port} 
+                                     dbname={$this->db_name} 
+                                     user={$this->user} 
+                                     password={$this->pwd}";
+    }
+  }
+?>
--- index.php
+++ index.php
@@ -1,26 +1,15 @@
-táblázat készítés php-ban. méret $sor x $oszlop<br>
-<?php
+<?= require_once 'init/db_connect.php'; ?>
 
-$sor=3;
-$oszlop=5;
-
-print '<table border=1>';
-
-$i = 0;
-ujrai:
-if ($i < $sor) {
-print '<tr>';
-$j = 0;
-ujraj:
-if ($j < $oszlop) {
-print '<td>'.$i.'.sor,'.$j.'.oszlop</td>';
-$j++;
-goto ujraj;
-}
-echo '</tr>';
-$i++;
-goto ujrai;
-}
-print '</table>';
-vege:
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Page</title>
+</head>
+<body>
+	<h1>Database Connection Parameters:</h1>
+	<ul>
+		<li>Database Name: <?= $db->getDbName() ?></li>
+		<li>User: <?=$db->getUser()  ?></li>
+	</ul>
+</body>
+</html>
